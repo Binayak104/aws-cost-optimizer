2620ba069fd187bfeb3a2cb01b9bbee42d156543
Binayak <63701876+Binayak104@users.noreply.github.com>
2025-11-11 22:59:50 +0530

    5

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,5 +1,26 @@
-variable "region" { default = "ap-south-1" }
-variable "project_name" { default = "aws-cost-optimizer" }
-variable "dry_run" { type = bool; default = true }
-variable "age_days" { type = number; default = 30 }
-variable "schedule" { default = "cron(0 2 ? * SUN *)" }
+variable "region" {
+  default = "ap-south-1"
+}
+
+variable "project_name" {
+  default = "aws-cost-optimizer"
+}
+
+variable "dry_run" {
+  type    = bool
+  default = true
+}
+
+variable "age_days" {
+  type    = number
+  default = 30
+}
+
+variable "whitelist_tags" {
+  type    = list(string)
+  default = ["Keep", "DoNotDelete"]
+}
+
+variable "schedule" {
+  default = "cron(0 2 ? * SUN *)"
+}
